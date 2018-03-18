3792ad80733e2d74ff0683ef71612c0f146d7f89
onlyo <onlyo@DESKTOP-QQOGV6F>
2018-03-18 15:17:52 +0700

    The first time.

---
+++ GGCloudGroup1/src/main/java/ggcloud/conference/controller/HomeController.java
@@ -0,0 +1,19 @@
+package ggcloud.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String Home() {
+		return "index";
+	}
+	
+	@GetMapping("/news")
+	public String News() {
+		return "News";
+	}
+	
+}
---
+++ GGCloudGroup1/src/main/java/ggcloud/conference/model/News.java
@@ -0,0 +1,9 @@
+package ggcloud.conference.model;
+
+public class News {
+	private int id;
+	private String content;
+	
+	
+
+}
