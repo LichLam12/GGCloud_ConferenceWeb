63b2bf23b33d20a84247e3c19d6b4175cbcfff2b
onlyo <onlyo@DESKTOP-QQOGV6F>
2018-03-25 15:56:47 +0700

    Done upload file.

--- GGCloudGroup1/src/main/java/ggcloud/conference/controller/HomeController.java
+++ GGCloudGroup1/src/main/java/ggcloud/conference/controller/HomeController.java
@@ -1,7 +1,10 @@
 package ggcloud.conference.controller;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
 
@@ -18,6 +23,26 @@ import ggcloud.conference.model.News;
 import ggcloud.conference.service.AboutService;
 import ggcloud.conference.service.EventService;
 import ggcloud.conference.service.NewsService;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+import com.google.api.services.drive.Drive;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -30,6 +55,8 @@ public class HomeController {
 	@Autowired
 	private EventService eventService;
 
+	public static String linkTai=null;
+	
 	@GetMapping("/home")
 	public String Home(HttpServletRequest request) {
 		request.setAttribute("aboutslist", aboutService.findAllAbout());
@@ -52,15 +79,41 @@ public class HomeController {
 
 	@GetMapping("/manage-news")
 	public String NewsManagementPage(Model model, HttpServletRequest request) {
-		/*
-		 * model.addAttribute("files", storageService .loadAll() .map(path ->
-		 * MvcUriComponentsBuilder .fromMethodName(HomeController.class, "serveFile",
-		 * path.getFileName().toString()) .build().toString())
-		 * .collect(Collectors.toList()));
-		 */
+		model.addAttribute("linkTai",linkTai);
 		return "managingnews";
 	}
 
+	@PostMapping("/uploadfile")
+	public void uploadFile(@RequestParam MultipartFile file, Model model,  HttpServletResponse response) throws Exception {
+		PrintWriter out = response.getWriter(); // Ä‘á»ƒ cho code gá»�n hÆ¡n
+
+		Drive service = getDriveService();
+		
+		File fileMetadata = new File();
+		fileMetadata.setTitle(file.getOriginalFilename());
+
+		java.io.File filePath = new java.io.File("upload-dir/" + file.getOriginalFilename());
+		filePath.createNewFile();
+		
+		FileContent mediaContent = new FileContent(file.getContentType(), filePath);
+		File f = service.files().insert(fileMetadata, mediaContent)
+		    .setFields("id")
+		    .execute();
+		
+		linkTai = "http://drive.google.com/open?id="+f.getId();
+
+		Path sourceFile = Paths.get("upload-dir/"+file.getOriginalFilename());
+		
+		response.setContentType("application/json");
+		// Import gson-2.2.2.jar
+		Gson gson = new Gson();
+		String objectToReturn = gson.toJson(linkTai); // Convert List -> Json
+		out.write(objectToReturn); // Ä�Æ°a Json tráº£ vá»� Ajax
+		out.flush();
+		
+/*		return "redirect:/manage-news";
+*/	}
+	
 	@GetMapping("/load-newslist")
 	public void ShowNewsList(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
@@ -189,120 +242,78 @@ public class HomeController {
 	}
 
 	/** Application name. */
-	/*
-	 * private static final String APPLICATION_NAME = "Drive API Java Quickstart";
-	 * 
-	 *//** Directory to store user credentials for this application. */
-	/*
-	 * private static final java.io.File DATA_STORE_DIR = new java.io.File(
-	 * System.getProperty("user.home"), ".credentials/drive-java-quickstart");
-	 * 
-	 *//** Global instance of the {@link FileDataStoreFactory}. */
-	/*
-	 * private static FileDataStoreFactory DATA_STORE_FACTORY;
-	 * 
-	 *//** Global instance of the JSON factory. */
-	/*
-	 * private static final JsonFactory JSON_FACTORY =
-	 * JacksonFactory.getDefaultInstance();
-	 * 
-	 *//** Global instance of the HTTP transport. */
-	/*
-	 * private static HttpTransport HTTP_TRANSPORT;
-	 * 
-	 *//**
-		 * Global instance of the scopes required by this quickstart.
-		 *
-		 * If modifying these scopes, delete your previously saved credentials at
-		 * ~/.credentials/drive-java-quickstart
-		 *//*
-			 * private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
-			 * //kiểu DRIVE toàn quyền (up file nào up)
-			 * 
-			 * static { try { HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			 * DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR); } catch
-			 * (Throwable t) { t.printStackTrace(); System.exit(1); } }
-			 * 
-			 * private final StorageService storageService;
-			 * 
-			 * @Autowired public HomeController(StorageService storageService) {
-			 * this.storageService = storageService; }
-			 * 
-			 * 
-			 * 
-			 * 
-			 * 
-			 * @GetMapping("/") public String listUploadedFiles(Model model) throws
-			 * IOException {
-			 * 
-			 * model.addAttribute("files", storageService .loadAll() .map(path ->
-			 * MvcUriComponentsBuilder .fromMethodName(HomeController.class, "serveFile",
-			 * path.getFileName().toString()) .build().toString())
-			 * .collect(Collectors.toList()));
-			 * 
-			 * return "uploadForm";
-			 * 
-			 * }
-			 * 
-			 * @GetMapping("/files/{filename:.+}")
-			 * 
-			 * @ResponseBody public ResponseEntity<Resource> serveFile(@PathVariable String
-			 * filename) {
-			 * 
-			 * Resource file = storageService.loadAsResource(filename); return
-			 * ResponseEntity .ok() .header(HttpHeaders.CONTENT_DISPOSITION,
-			 * "attachment; filename=\""+file.getFilename()+"\"") .body(file); }
-			 * 
-			 * @PostMapping("/") public String handleFileUpload(@RequestParam("file")
-			 * MultipartFile file, RedirectAttributes redirectAttributes) throws Exception {
-			 * 
-			 * storageService.store(file);
-			 * 
-			 * //google drive Drive service = getDriveService();
-			 * 
-			 * File fileMetadata = new File(); //kdl file của gg
-			 * fileMetadata.setTitle(file.getOriginalFilename()); //file ở đây là dòng 115,
-			 * lấy tên chính thống của file đã up //Upload xg tạo file mới dựa vào đường dẫn
-			 * này java.io.File filePath = new
-			 * java.io.File("upload-dir/"+file.getOriginalFilename()); //Lấy kdl của file
-			 * (image/docs...) FileContent mediaContent = new
-			 * FileContent(file.getContentType(),filePath); File f =
-			 * service.files().insert(fileMetadata, mediaContent) //tìm all dvu insert vào
-			 * file mới với id để thực thi .setFields("id") .execute();
-			 * //System.out.println("File ID: " + f.getId()+" | "+f.getWebContentLink());
-			 * //message trong file html redirectAttributes.addFlashAttribute("message",
-			 * "You successfully uploaded " + file.getOriginalFilename() );
-			 * redirectAttributes.addFlashAttribute("linktai",
-			 * "https://drive.google.com/open?id="+f.getId()); return "redirect:/"; } public
-			 * static Credential authorize() throws Exception { // Load client secrets.
-			 * InputStream in =
-			 * HomeController.class.getResourceAsStream("/client_secret.json");
-			 * GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY,
-			 * new InputStreamReader(in));
-			 * 
-			 * // Build flow and trigger user authorization request.
-			 * GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-			 * HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-			 * .setDataStoreFactory(DATA_STORE_FACTORY) .setAccessType("offline") .build();
-			 * Credential credential = new AuthorizationCodeInstalledApp( flow, new
-			 * LocalServerReceiver()).authorize("user"); System.out.println(
-			 * "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath()); return
-			 * credential; }
-			 * 
-			 * 
-			 * Build and return an authorized Drive client service.
-			 * 
-			 * @return an authorized Drive client service
-			 * 
-			 * @throws Exception
-			 * 
-			 * public static Drive getDriveService() throws Exception { Credential
-			 * credential = authorize(); return new Drive.Builder( HTTP_TRANSPORT,
-			 * JSON_FACTORY, credential) .setApplicationName(APPLICATION_NAME) .build(); }
-			 * 
-			 * @ExceptionHandler(StorageFileNotFoundException.class) public ResponseEntity
-			 * handleStorageFileNotFound(StorageFileNotFoundException exc) { return
-			 * ResponseEntity.notFound().build(); }
-			 */
+    private static final String APPLICATION_NAME =
+        "Drive API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     */
+    private static final List<String> SCOPES =
+        Arrays.asList(DriveScopes.DRIVE);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws Exception 
+     */
+    public static Credential authorize() throws Exception {
+        // Load client secrets.
+        InputStream in =
+            HomeController.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Drive client service.
+     * @return an authorized Drive client service
+     * @throws Exception 
+     */
+    public static Drive getDriveService() throws Exception {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
 }
